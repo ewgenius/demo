84d1bc252aae3fd4c3e6e2f75397a5e119b38b42
Evgenii Khramkov <hey@ewgenius.me>
2023-08-31 14:18:36 +0900

    try to fix

--- hello_world_golang/spice_function.go
+++ hello_world_golang/spice_function.go
@@ -11,17 +11,8 @@ import (
 func HelloWorldGo(ctx *function.FunctionCtx, duckDb *sql.DB, client *gospice.SpiceClient) error {
 	fmt.Println("Hello from Spice Go runtime!")
 
-	// Temporary step
-	_, err := duckDb.ExecContext(ctx, `
-	create table output.ewgenius.demo.hello_world_golang (
-		block_number bigint,
-		greeting TEXT
-	);`)
-	if err != nil {
-		return err
-	}
 
-	_, err = duckDb.ExecContext(ctx, "INSERT INTO output.ewgenius.demo.hello_world_golang (block_number, greeting) VALUES ($1, $2);", ctx.BlockNumber(), "Hello from Spice Go runtime!")
+	_, err = duckDb.ExecContext(ctx, "INSERT INTO output.ewgenius_demo_hello_world_golang (block_number, greeting) VALUES ($1, $2);", ctx.BlockNumber(), "Hello from Spice Go runtime!")
 	if err != nil {
 		return err
 	}
